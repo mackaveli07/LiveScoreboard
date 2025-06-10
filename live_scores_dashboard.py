ae6fb833c6b0e7919b1d235070212875b8112307
mackaveli07 <60677021+mackaveli07@users.noreply.github.com>
2025-06-10 14:30:13 -0500

    Update live_scores_dashboard.py

--- live_scores_dashboard.py
+++ live_scores_dashboard.py
@@ -347,16 +347,13 @@ def display_scores(sport_name, date):
                                         st.markdown("**Field Position:** Unknown")
                 else:
                     st.markdown(f"Inning: {game['period']}")
-                diamond_html = f'''
-<div class="diamond">
-    <div class="base second {'occupied' if game['on_second'] else ''}"></div>
-    <div class="base third {'occupied' if game['on_third'] else ''}"></div>
-    <div class="base first {'occupied' if game['on_first'] else ''}"></div>
-</div>
+                    diamond_html = f'''
+    <div class="diamond">
+        <div class="base second {'occupied' if game['on_second'] else ''}"></div>
+        <div class="base third {'occupied' if game['on_third'] else ''}"></div>
+        <div class="base first {'occupied' if game['on_first'] else ''}"></div>
+    </div>
 '''
-st.markdown(diamond_html, unsafe_allow_html=True)
-'''
-                    
                     st.markdown(diamond_html, unsafe_allow_html=True)
                     st.markdown(f"**Outs:** {game['outs']}")
                     st.markdown(f"**Balls:** {game['balls']}  **Strikes:** {game['strikes']}")
