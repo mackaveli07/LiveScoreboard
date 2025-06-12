b8d11df0d832aaa2eaaf56457cc67ad7491fb9e5
mackaveli07 <60677021+mackaveli07@users.noreply.github.com>
2025-06-12 14:53:51 -0500

    Update live_scores_dashboard.py

--- live_scores_dashboard.py
+++ live_scores_dashboard.py
@@ -48,9 +48,9 @@ st.markdown("""
         background-color: #228B22;
         border-color: #145214;
     }
-    .first { bottom: 15%; right: 10%; transform: translate(50%, 50%) rotate(45deg); }
+    .first { bottom: 15%; right: 10%; transform: translate(50%, 50%) rotate(0deg); }
     .second { top: 0; left: 50%; transform: translate(-50%, -50%) rotate(45deg); }
-     .third { bottom: 15%; left: 10%; transform: translate(-50%, 50%) rotate(45deg); }
+     .third { bottom: 15%; left: 10%; transform: translate(-50%, 50%) rotate(90deg); }
     .score-blink {
         animation: blinkScore 1s step-start 0s infinite;
     }
