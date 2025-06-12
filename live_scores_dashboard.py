9f9722c4819e8b5564c575ef4b9c83eeee60b0ae
mackaveli07 <60677021+mackaveli07@users.noreply.github.com>
2025-06-12 11:06:30 -0500

    Update live_scores_dashboard.py

--- live_scores_dashboard.py
+++ live_scores_dashboard.py
@@ -406,7 +406,7 @@ def display_scores(sport_name, date, scores):
             f"<div class='team-score-wrapper' style='background: linear-gradient(135deg, {color1}, {color1b})'>"
             f"<div class='team-name'>{t1['name']}</div>{popup1}"
             f{'<div class="team-score-box score-blink">' if b1 else '<div class="team-score-box">'}{t1['score']}</div>"
-            "</div>"
+            </div>"
         )
 
         score2_html = (
