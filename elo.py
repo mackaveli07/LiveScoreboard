ba55b2a718fb32a0e336a5095dc6ceffcf73f1b5
mackaveli07 <60677021+mackaveli07@users.noreply.github.com>
2025-06-20 13:31:27 -0500

    Update elo.py

--- elo.py
+++ elo.py
@@ -2,26 +2,26 @@
 # elo.py
 
 class EloRating:
-    def __init__(self, initial_rating=1500, k=20):
-        self.ratings = {}
+    def __init__(self, k=20, base_rating=1500):
         self.k = k
-        self.initial_rating = initial_rating
+        self.ratings = {}
+        self.base_rating = base_rating
 
     def get_rating(self, team):
-        return self.ratings.get(team, self.initial_rating)
+        return self.ratings.get(team, self.base_rating)
 
-    def expected_score(self, rating_a, rating_b):
-        return 1 / (1 + 10 ** ((rating_b - rating_a) / 400))
+    def expected_result(self, rating1, rating2):
+        return 1 / (1 + 10 ** ((rating2 - rating1) / 400))
 
-    def update_ratings(self, team_a, team_b, result):
-        rating_a = self.get_rating(team_a)
-        rating_b = self.get_rating(team_b)
+    def update_ratings(self, team1, team2, result):
+        r1 = self.get_rating(team1)
+        r2 = self.get_rating(team2)
 
-        expected_a = self.expected_score(rating_a, rating_b)
-        expected_b = self.expected_score(rating_b, rating_a)
+        expected1 = self.expected_result(r1, r2)
+        expected2 = self.expected_result(r2, r1)
 
-        self.ratings[team_a] = rating_a + self.k * (result - expected_a)
-        self.ratings[team_b] = rating_b + self.k * ((1 - result) - expected_b)
+        self.ratings[team1] = r1 + self.k * (result - expected1)
+        self.ratings[team2] = r2 + self.k * ((1 - result) - expected2)
 
     def get_all_ratings(self):
-        return dict(sorted(self.ratings.items(), key=lambda x: -x[1]))
+        return dict(sorted(self.ratings.items(), key=lambda item: item[1], reverse=True))
