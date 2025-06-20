d64c23f7ae9556427111bc115b64ba1e5a312f69
mackaveli07 <60677021+mackaveli07@users.noreply.github.com>
2025-06-20 15:56:08 -0500

    Update live_scores_dashboard.py

--- live_scores_dashboard.py
+++ live_scores_dashboard.py
@@ -169,13 +169,7 @@ for i, tab_key in enumerate(tabs_keys):
                                         🏀 <strong>Quarter:</strong> {info.get('quarter', 'N/A')}<br/>
                                         ⏱️ <strong>Clock:</strong> {info.get('clock', '')}
                                         <div class='court'>
-                                            <div class='half-court'></div>
-                                            <div class='top-hoop'></div>
-                                            <div class='bottom-hoop'></div>
-                                            <div class='top-ft-arc'></div>
-                                            <div class='bottom-ft-arc'></div>
-                                            <div class='top-3pt-arc'></div>
-                                            <div class='bottom-3pt-arc'></div>
+                                           
                                         </div>
                                     </div>
                             """, unsafe_allow_html=True)
